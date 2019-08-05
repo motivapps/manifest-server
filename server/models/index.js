9a7bc058e90c4e16b1d5a5d0fb29d0c204c213a4
clydebates <a.bates1993@gmail.com>
2019-08-05 15:51:53 -0500

    Correct database connection

--- server/models/index.js
+++ server/models/index.js
@@ -10,14 +10,14 @@ const sequelize = new Sequelize(
   }
 )
 
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 const models = {
   Users: sequelize.import('./tables/users'),
