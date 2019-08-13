f27462116ad7c709f3b26943486bd409389d0daa
Rachel Brandsness <brandsnessr@gmail.com>
2019-08-13 16:15:50 -0500

    Store goal to database

--- server/index.js
+++ server/index.js
@@ -81,7 +81,7 @@ app.post('/signup', (req, res) => {
         : User.create({ name, auth0_id, picture }); 
     })
     // adds a test goal to the first loser to login!  :^ P
-    .then(() => loadDataGoals(testGoals))
+  //  .then(() => loadDataGoals(testGoals))
     .catch(err => console.error(err));
 });
 
@@ -231,6 +231,28 @@ app.post('/pushtoken', (req, res) => {
 app.post('/user/goals', (req, res) => {
   console.log('inside pushtoken route');
   console.log('goals post req:', req.body);
+  
+  Goal.create({ 
+    id_user: req.body.userId,
+    vice: req.body.viceName,
+    goal_name: req.body.goalName,
+    goal_item: req.body.goalItem,
+    goal_cost: req.body.goalAmount,
+    vice_freq: req.body.viceFrequency,
+    vice_price: req.body.vicePrice,
+  })
+});
+
+app.get('/user/:auth0_id', (req, res) => {
+  console.log('inside user id get');
+  User.findOne({ where: { auth0_id: req.params.auth0_id } })
+  .then((response) => {
+    console.log('then response:', response);
+    res.status(200).send(response);
+  })
+  .catch((err) => {
+    console.log('UserId get error:', err);
+  })
 })
 
 db.sync({ force: false }).then(() => {
