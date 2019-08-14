9ffa6e1297a349513db07c5b18591c0cc0ca9292
clydebates <a.bates1993@gmail.com>
2019-08-14 14:47:38 -0500

    Linting fixes

--- server/models/tables/users.js
+++ server/models/tables/users.js
@@ -1,22 +1,22 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-module.exports = db.define('users',{
-  "name": Sequelize.STRING,
-  "auth0_id": {
+module.exports = db.define('users', {
+  'name': Sequelize.STRING,
+  'auth0_id': {
     type: Sequelize.STRING,
     unique: true
   },
-  "picture": Sequelize.STRING,
-  "access_token": Sequelize.STRING,
-  "item_id": Sequelize.STRING,
-  "session_token": Sequelize.STRING,
-  "device_token": Sequelize.STRING,
-  "totalSaved": Sequelize.INTEGER,
-  "transfer_schedule": {
+  'picture': Sequelize.STRING,
+  'access_token': Sequelize.STRING,
+  'item_id': Sequelize.STRING,
+  'session_token': Sequelize.STRING,
+  'device_token': Sequelize.STRING,
+  'totalSaved': Sequelize.INTEGER,
+  'transfer_schedule': {
     type: Sequelize.ENUM,
     values: ['a', 'b', 'c']
   },
-  "games": Sequelize.INTEGER,
-  }, { db }
+  'games': Sequelize.INTEGER,
+}, { db }
 );
